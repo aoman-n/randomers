8c99c70dd0b2d407036aa889804b6e315353a1f4
aoba <ipbdipbd00@gmail.com>
2019-05-10 00:39:44 +0900

    Remove unnecessary files

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import { createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import { rootReducer } from './modules';
+// import { rootReducer } from './modules';
 import App from './App';
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  // <Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  // </Provider>,
   document.getElementById('root'),
 );
